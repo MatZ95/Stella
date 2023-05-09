ec0de5500bf11356ed9dea4c9f15f5c3e17355b6
grim1990 <grim1990@gmail.com>
2023-05-09 16:06:54 +0200

    edit form created

--- routes/stars.js
+++ routes/stars.js
@@ -14,7 +14,7 @@ router.post("/new", async (req, res) => {
 
   let qry = { name: starsName };
 
-  let searchResult = await stars.findOne(qry).then(async (userData) => {
+  await stars.findOne(qry).then(async (userData) => {
     if (!userData) {
       // Można dodać gwiazdę
       let newStars = new schemas.stars({
@@ -39,6 +39,39 @@ router.delete('/:id', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
+  }
+});
+
+router.get('/:id/edit', async (req, res, next) => {
+  try {
+    let stars = schemas.stars;
+    const star = await stars.findById(req.params.id);
+
+    if (!star) {
+      throw new Error('Star not found');
+    }
+
+    res.render('edit', { title: 'Edit Star', star: star });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.post('/:id/edit', async (req, res) => {
+  try {
+    const starId = req.params.id;
+    const update = { name: req.body.starsName, type: req.body.starsType };
+    const UpdatedStar = await schemas.stars.findByIdAndUpdate(starId, update, { new: true });
+
+    if (!UpdatedStar) {
+      throw new Error('Star not found');
+    }
+
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error updating star');
   }
 });
 
