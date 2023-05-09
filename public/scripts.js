01b10e14c5e18f59495b653360dc6f6a06fb2754
grim1990 <grim1990@gmail.com>
2023-05-09 16:32:32 +0200

    rework edit function

---
+++ public/scripts.js
@@ -0,0 +1,8 @@
+function showEditForm(id) {
+    var form = document.getElementById('edit-form-' + id);
+    if (form.style.display === "none") {
+      form.style.display = "block";
+    } else {
+      form.style.display = "none";
+    }
+  }
