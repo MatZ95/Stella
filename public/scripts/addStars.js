6ba3eed364112855757a535031c5bca6ce40e3a0
grim1990 <grim1990@gmail.com>
2023-05-11 16:49:29 +0200

    randomize star colors

--- public/scripts/addStars.js
+++ public/scripts/addStars.js
@@ -13,13 +13,24 @@ function addStars(numStars) {
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
 
+    const starImages = [
+        '/images/stars/darkstar.png',
+        '/images/stars/redstar.png',
+        '/images/stars/whitestar.png',
+        '/images/stars/yellowstar.png'
+    ];
+
     fetch('/stars/stardata')
         .then(res => res.json())
         .then(data => {
             for (let i = 0; i < numStars; i++) {
                 const star = document.createElement('img');
                 star.classList.add('star');
-                star.src = '/images/star-image.png';
+
+                const randomImageIndex = Math.floor(Math.random() * starImages.length);
+                const starImagePath = starImages[randomImageIndex];
+                star.src = starImagePath;
+
                 star.style.left = Math.floor(Math.random() * screenWidth) + 'px';
                 star.style.top = Math.floor(Math.random() * screenHeight) + 'px';
 
