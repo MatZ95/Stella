25210d67f46c697498a32ea6acca7b23d77dcd84
grim1990 <grim1990@gmail.com>
2023-05-11 12:02:39 +0200

    add moon as background image

--- public/scripts/moonPhases.js
+++ public/scripts/moonPhases.js
@@ -1,17 +1,20 @@
-const moonDiv = document.getElementById('moon');
-const moonRange = document.getElementById('moon-phase');
-const moonImage = document.getElementById('moon-image');
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('container');
+    const moonRange = document.getElementById('moon-phase');
 
-moonRange.addEventListener('input', () => {
-    const phase = moonRange.value;
-    const moonImagePath = getMoonImagePath(phase);
-    moonDiv.style.backgroundImage = `url(${moonImagePath})`;
-    moonImage.src = moonImagePath;
-});
+    moonRange.addEventListener('input', () => {
+      const phase = moonRange.value;
+      const moonImagePath = getMoonImagePath(phase);
+      container.style.backgroundImage = `url(${moonImagePath})`;
+    });
 
-function getMoonImagePath(phase) {
-    const phaseNames = ['new-moon', 'waxing-crescent', 'first-quarter', 'waxing-gibbous', 'full-moon', 'waning-gibbous', 'last-quarter', 'waning-crescent'];
-    const phaseIndex = Math.round(phase / 4);
-    const phaseName = phaseNames[phaseIndex % 8];
-    return `/images/moon-phases/${phaseName}.png`;
-}
+    function getMoonImagePath(phase) {
+      const phaseNames = ['new-moon', 'waxing-crescent', 'first-quarter', 'waxing-gibbous', 'full-moon', 'waning-gibbous', 'last-quarter', 'waning-crescent'];
+      const phaseIndex = Math.round(phase / 4);
+      const phaseName = phaseNames[phaseIndex % 8];
+      return `/images/moon-phases/${phaseName}.png`;
+    }
+
+    const defaultImagePath = getMoonImagePath(0);
+    container.style.backgroundImage = `url(${defaultImagePath})`;
+  });
