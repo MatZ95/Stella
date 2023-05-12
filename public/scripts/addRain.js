7a1b3392bf25099de90d9cd6665d2429d959cae6
grim1990 <grim1990@gmail.com>
2023-05-12 19:30:54 +0200

    add rain

---
+++ public/scripts/addRain.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const rainVideo = document.getElementById('rain-video');
+    const rainSlider = document.getElementById('rain-slider');
+    const cloudinessSlider = document.getElementById('cloudiness');
+
+    rainSlider.addEventListener('input', function () {
+        if (rainSlider.value === '0' || cloudinessSlider.value === '0') {
+            rainVideo.style.display = 'none';
+        } else {
+            rainVideo.style.display = 'block';
+        }
+    });
+
+    rainVideo.style.display = 'none';
+});
