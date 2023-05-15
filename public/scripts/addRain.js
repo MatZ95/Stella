cf7d2a80bf37050961c0f170571d1043f76d26cd
grim1990 <grim1990@gmail.com>
2023-05-15 14:21:49 +0200

    correct rain slider

--- public/scripts/addRain.js
+++ public/scripts/addRain.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     rainSlider.addEventListener('input', function () {
         if (rainSlider.value === '0' || cloudinessSlider.value === '0') {
+            rainSlider.value = '0'
             rainVideo.style.display = 'none';
         } else {
             rainVideo.style.display = 'block';
